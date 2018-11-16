c86d12ce0f55efac0bbab5720c4d81f95d890463
Djamel <djamel.rezki@theb2group.net>
2018-11-16 09:19:35 +0100

    sources added

---
+++ src/main/java/com/b2/generator/Main.java
@@ -0,0 +1,33 @@
+package com.b2.generator;
+
+import com.github.lalyos.jfiglet.FigletFont;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+
+@SpringBootApplication
+public class Main {
+
+  public static void main(String[] args) {
+    try {
+      String gAscii = FigletFont.convertOneLine("Gresham");
+
+      System.out.print("Working");
+
+      for (int i = 0; i < 5; i++) {
+        Thread.sleep(300);
+        System.out.print(".");
+      }
+
+      BufferedWriter writer = new BufferedWriter(new FileWriter("output.txt"));
+      writer.write(gAscii);
+      writer.close();
+
+      System.out.println(" Done !");
+
+    } catch (Exception e) {
+      System.out.println("Exception : " + e.getMessage());
+    }
+  }
+}
